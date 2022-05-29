7ae0c053df9ef7edef933674a6fba889929974b2
dioanres <dioanres@gmail.com>
2022-05-29 08:54:30 +0700

    d

--- app/Views/web/layouts/header.php
+++ app/Views/web/layouts/header.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="Toyota, Mobile, Mobil, Car, Astra, Mobilbaru">
     <meta name="author" content="">
 
     <title> Toyota </title>
